13f471c602f667779f796d16f8cf50f88422af55
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-29 12:27:56 +0700

    inlis: worksheetsub source

---
+++ protected/modules/inlis/views/o/worksheet_sub/admin_view.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Inlis Worksheet Subs (inlis-worksheet-sub)
+ * @var $this WorksheetsubController
+ * @var $model InlisWorksheetSub
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 29 March 2016, 10:00 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Inlis Worksheet Subs'=>array('manage'),
+		$model->Name,
+	);
+?>
+
+<div class="dialog-content">
+	<?php $this->widget('application.components.system.FDetailView', array(
+		'data'=>$model,
+		'attributes'=>array(
+			array(
+				'name'=>'ID',
+				'value'=>$model->ID,
+				//'value'=>$model->ID != '' ? $model->ID : '-',
+			),
+			array(
+				'name'=>'Name',
+				'value'=>$model->Name,
+				//'value'=>$model->Name != '' ? $model->Name : '-',
+			),
+			array(
+				'name'=>'Main_Worksheet_ID',
+				'value'=>$model->Main_Worksheet_ID,
+				//'value'=>$model->Main_Worksheet_ID != '' ? $model->Main_Worksheet_ID : '-',
+			),
+		),
+	)); ?>
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::button(Yii::t('phrase', 'Close'), array('id'=>'closed')); ?>
+</div>
