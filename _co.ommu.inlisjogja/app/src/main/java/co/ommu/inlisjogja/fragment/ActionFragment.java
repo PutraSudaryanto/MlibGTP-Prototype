055c3bda9b956d9e68a7c6bf585522b0c3e99c66
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-07 03:24:44 +0700

    apps: fragment

---
+++ _co.ommu.inlisjogja/app/src/main/java/co/ommu/inlisjogja/fragment/ActionFragment.java
@@ -0,0 +1,73 @@
+package co.ommu.inlisjogja.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.ommu.inlisjogja.R;
+
+public class ActionFragment extends Fragment {
+    Context context;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_action, container, false);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new ViewFragment(), getResources().getString(R.string.action_views));
+        adapter.addFragment(new BookmarkFragment(), getResources().getString(R.string.action_bookmarks));
+        adapter.addFragment(new LikeFragment(), getResources().getString(R.string.action_likes));
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+}
---
+++ _co.ommu.inlisjogja/app/src/main/java/co/ommu/inlisjogja/fragment/ViewFragment.java
@@ -0,0 +1,19 @@
+package co.ommu.inlisjogja.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.ommu.inlisjogja.R;
+
+public class ViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view, container, false);
+
+        return view;
+    }
+}
