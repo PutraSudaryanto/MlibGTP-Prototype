cad08ea5e50221ea6355e943ffd63c00a951f0e1
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-07 04:56:21 +0700

    apps: modify action tabs

--- _co.ommu.inlisjogja/app/src/main/java/co/ommu/inlisjogja/fragment/ActionFragment.java
+++ _co.ommu.inlisjogja/app/src/main/java/co/ommu/inlisjogja/fragment/ActionFragment.java
@@ -43,31 +43,31 @@ public class ActionFragment extends Fragment {
     }
 
     static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragments = new ArrayList<>();
-        private final List<String> mFragmentTitles = new ArrayList<>();
+        private final List<Fragment> oFragment = new ArrayList<>();
+        private final List<String> oFragmentTitle = new ArrayList<>();
 
         public Adapter(FragmentManager fm) {
             super(fm);
         }
 
         public void addFragment(Fragment fragment, String title) {
-            mFragments.add(fragment);
-            mFragmentTitles.add(title);
+            oFragment.add(fragment);
+            oFragmentTitle.add(title);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return mFragments.get(position);
+            return oFragment.get(position);
         }
 
         @Override
         public int getCount() {
-            return mFragments.size();
+            return oFragment.size();
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return mFragmentTitles.get(position);
+            return oFragmentTitle.get(position);
         }
     }
 }
