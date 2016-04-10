408cf2ba45574f066d813e046bdbc314c865e71e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-10 09:17:16 +0700

    track: add catalog info in query

--- app/src/main/java/co/ommu/inlisjogja/inlis/model/TrackModel.java
+++ app/src/main/java/co/ommu/inlisjogja/inlis/model/TrackModel.java
@@ -3,4 +3,9 @@ package co.ommu.inlisjogja.inlis.model;
 public class TrackModel {
 	public String catalog_id;
 	public String creation_date;
+	public String title;
+	public String author;
+	public String publisher;
+	public String publish_location;
+	public String publish_year;
 }
