e433757adf6d7f57d16f89745c307ac66e7609cb
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-10 08:36:10 +0700

    drop duplicate model

--- app/src/main/java/co/ommu/inlisjogja/inlis/model/FavouriteListModel.java
+++
@@ -1,6 +0,0 @@
-package co.ommu.inlisjogja.inlis.model;
-
-public class FavouriteListModel {
-	public String catalog_id;
-	public String creation_date;
-}
--- app/src/main/java/co/ommu/inlisjogja/inlis/model/ViewListModel.java
+++
@@ -1,6 +0,0 @@
-package co.ommu.inlisjogja.inlis.model;
-
-public class ViewListModel {
-	public String catalog_id;
-	public String creation_date;
-}
