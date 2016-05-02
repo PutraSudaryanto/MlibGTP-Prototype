960f8492fdf948f0a627a7603d9464a96c0e7156
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-02 07:27:29 +0700

    modify layout

--- app/src/main/java/co/ommu/inlisjogja/WelcomeDrawerActivity.java
+++ app/src/main/java/co/ommu/inlisjogja/WelcomeDrawerActivity.java
@@ -147,11 +147,8 @@ public class WelcomeDrawerActivity extends AppCompatActivity
 
         if (id == R.id.nav_home) {
            getSupportFragmentManager().beginTransaction().replace(R.id.container, new WelcomeFragment()).commit();
-
-
         } else if (id == R.id.nav_track) {
            getSupportFragmentManager().beginTransaction().replace(R.id.container, new TrackTabMemberFragment(0)).commit();
-
         } else if (id == R.id.nav_track_favourite) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new TrackMemberFragment("favourites")).commit();
         } else {
